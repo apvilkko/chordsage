1cf2b8ea6f00d4c478c99c6f6073488ef3d4a3ec
Antti Pekka Vilkko <apvilkko@gmail.com>
2016-02-03 09:39:10 +0200

    fix watch and livereload

--- gulpfile.js
+++ gulpfile.js
@@ -8,30 +8,8 @@ var babel = require('babelify');
 var connect = require('gulp-connect');
 var glob = require('glob');
 var jasmine = require('gulp-jasmine');
+var gulpif = require('gulp-if');
 var specFiles = './spec/*.js';
-
-function runTests() {
-  return gulp.src('./build/specs.js').pipe(jasmine());
-}
-
-function compileTests() {
-  var filenames = glob.sync(specFiles);
-  return browserify({
-    entries: filenames,
-    debug: true
-  })
-  .on('error', function (err) {
-    console.error(err);
-    this.emit('end');
-   })
-  .transform(babel)
-  .bundle()
-  .pipe(source('specs.js'))
-  .pipe(buffer())
-  .pipe(sourcemaps.init({ loadMaps: true }))
-  .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest('./build'));
-}
 
 function compile(watch, sources, target, tests) {
   var browserifyTask = browserify({
@@ -41,22 +19,25 @@ function compile(watch, sources, target, tests) {
   var bundler = watch ? watchify(browserifyTask) : browserifyTask;
 
   function rebundle() {
-    bundler.bundle()
+    return bundler.bundle()
       .on('error', function(err) { console.error(err); this.emit('end'); })
       .pipe(source(target))
       .pipe(buffer())
       .pipe(sourcemaps.init({ loadMaps: true }))
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./build'));
+      .pipe(gulp.dest('./build'))
+      .pipe(gulpif(watch, connect.reload()))
+      .on('end', () => {
+        if (tests) {
+          runTests();
+        }
+      });
   }
 
   if (watch) {
     bundler.on('update', function() {
       console.log('-> bundling...');
       rebundle();
-      if (tests) {
-        runTests();
-      }
     });
   }
 
@@ -71,15 +52,19 @@ function testWatch() {
   return compile(true, glob.sync(specFiles), 'specs.js', true);
 }
 
+function runTests() {
+  return gulp.src('./build/specs.js').pipe(jasmine());
+}
+
+function compileTests() {
+  return compile(false, glob.sync(specFiles), 'specs.js', true);
+}
+
 gulp.task('build', function() { return compile(false, './src/index.js', 'build.js'); });
 gulp.task('build-tests', function() { return compileTests(); });
 gulp.task('run-tests', ['build-tests'], function() { return runTests(); });
 gulp.task('watch', function() { return watch(); });
-gulp.task('webserver', function() {
-  connect.server({
-    livereload: true
-  });
-});
+gulp.task('webserver', function() { connect.server({livereload: true}); });
 gulp.task('test-watch', function () { return testWatch(); });
 gulp.task('default', ['webserver', 'watch']);
 gulp.task('test', ['build-tests', 'run-tests']);
